3d050aef1520451cac28d8a559aed27bdebbe6e3
kevin <kevonegrin@gmail.com>
2023-02-27 19:56:11 -0300

    primer entregable backend

---
+++ appNode/Class/index.js
@@ -0,0 +1,46 @@
+class TicketManager{
+    getEventos(){
+        []
+    }
+    agregarEvento(){
+        nombre="",
+        lugar="",
+        precio="",
+        capacidad=50,
+        fecha=""
+        participantes=[
+            {
+
+            }
+        ]     
+    }
+    agregarUsario(){
+        participantes.push
+    }
+
+
+    #precioBaseDeGanancia = precio + 0.15
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = ProductManager 
